1673b49b84560aff45c4c8a62434f887dfdb44d0
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-06 20:33:37 +0000

    Automatically backed up by Learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -2,9 +2,9 @@ export class RegistrationForm extends React.Component {
   render() {
     return (
       <form>
-        <input type="text">
-        <input type="password">
-        <button type="submit">
+        <input type="text" />
+        <input type="password" />
+        <button type="submit" />
       </form>
     )
   }
